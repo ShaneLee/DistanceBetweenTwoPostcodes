0d8021534c2084b6f9cc9c83ba85728c409324c6
ShaneLee <shaneleephoto@gmail.com>
2018-11-26 15:27:44 +0000

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,28 @@
+document.addEventListener('keypress', function (e) {
+  if (13 == e.keyCode) {
+    var postcode_1 = String(document.getElementById("postcode_1").value);
+    var postcode_2 = String(document.getElementById("postcode_2").value);
+    console.log(postcode_1);
+    console.log(postcode_2);
+    getDistanceMiles(postcode_1, postcode_2);
+
+  }
+});
+
+function getDistanceMiles(postcode_1, postcode_2) {
+  const service = new google.maps.DistanceMatrixService;
+  service.getDistanceMatrix({
+      origins: [postcode_1],
+      destinations: [postcode_2],
+      travelMode: 'DRIVING',
+      unitSystem: google.maps.UnitSystem.IMPERIAL
+  }, callback);
+}
+
+function callback(response, status) {
+  console.log(response);
+  var distance = response.rows[0].elements[0].distance.text;
+  var duration = response.rows[0].elements[0].duration.text;
+  console.log(distance);
+  console.log(duration);
+}
